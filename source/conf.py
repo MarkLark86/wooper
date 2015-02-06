6c586a06aa2005622da7dec8bcff3d1974310840
actionless <actionless.loveless@gmail.com>
2015-02-06 13:33:15 +0100

    chore(docs): show full toc on each page

--- source/conf.py
+++ source/conf.py
@@ -33,6 +33,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
+    'sphinxcontrib.fulltoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
